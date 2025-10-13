2b6f14939730b18340e4a7145c12db886e2822c8
Yard Sale Tracker <yardsale@example.com>
2025-10-13 08:26:29 -0400

    CI: fix ESLint plugin (eslint-plugin-testing-library) and update ESLint config; lockfile refresh required

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import typescript from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import testingLibrary from '@testing-library/eslint-plugin'
+import testingLibrary from 'eslint-plugin-testing-library'
 import vitest from 'eslint-plugin-vitest'
 
 export default [

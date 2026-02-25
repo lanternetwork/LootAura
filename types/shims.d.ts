1204499af56f9009795487ffc782321bbceb14f5
Auto <auto@cursor.sh>
2026-02-25 13:56:10 -0500

    fix: remove duplicate mapbox__point-geometry declaration from shims.d.ts

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -1,9 +1,3 @@
 declare module 'react-map-gl';
 
-// Type stub for mapbox__point-geometry (transitive dependency of react-map-gl/mapbox-gl)
-// Prevents TS2688: Cannot find type definition file for 'mapbox__point-geometry'
-declare module 'mapbox__point-geometry' {
-  const _: any;
-  export = _;
-}
 

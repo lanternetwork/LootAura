1b65fb653821b55cc3aa99059e983a3f9ea08dc3
Auto <auto@cursor.sh>
2026-02-25 13:30:51 -0500

    fix: change moduleResolution to node to resolve mapbox__point-geometry type error
    - Changed moduleResolution from 'bundler' to 'node' in tsconfig.json
    - Simplifies type declaration for mapbox__point-geometry to simple declare module
    - Fixes TS2688: Cannot find type definition file for implicit type library
    - 'node' resolution properly recognizes our custom type declarations in types/

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -2,7 +2,5 @@ declare module 'react-map-gl';
 
 // Type stub for mapbox__point-geometry (transitive dependency of react-map-gl/mapbox-gl)
 // Prevents TS2688: Cannot find type definition file for 'mapbox__point-geometry'
-declare module 'mapbox__point-geometry' {
-  // Empty module - this package doesn't export types
-}
+declare module 'mapbox__point-geometry';
 
